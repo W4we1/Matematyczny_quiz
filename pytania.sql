086bdbdc27429b3fa50e9c7ef671abd875ea99a0
W4we1 <Wawel2004@gmail.com>
2022-10-26 15:28:57 +0200

    nie wiem co napisac

---
+++ pytania.sql
@@ -0,0 +1,54 @@
+#pytania
+
+INSERT INTO questions (description) VALUES ("Ile to 2*2?");
+INSERT INTO questions (description) VALUES ("Ile to 2*4?");
+INSERT INTO questions (description) VALUES ("Ile to 2*50?");
+INSERT INTO questions (description) VALUES ("Ile to 15+15+15?");
+INSERT INTO questions (description) VALUES ("Ile to 3+6+9?");
+INSERT INTO questions (description) VALUES ("Ile to 50-40?");
+INSERT INTO questions (description) VALUES ("Ile to 50-40+50?");
+INSERT INTO questions (description) VALUES ("Ile to 6/3?");
+INSERT INTO questions (description) VALUES ("Ile to 2+2*2");
+INSERT INTO questions (description) VALUES ("Ile to 60+60-20?");
+
+#odpowiedzi
+
+INSERT INTO answers (descriptions, is_right, questions_id) VALUES ("2", "0", "1");
+INSERT INTO answers (descriptions, is_right, questions_id) VALUES ("4", "1", "1");
+INSERT INTO answers (descriptions, is_right, questions_id) VALUES ("12", "0", "1");
+INSERT INTO answers (descriptions, is_right, questions_id) VALUES ("4", "0", "2");
+INSERT INTO answers (descriptions, is_right, questions_id) VALUES ("8", "1", "2");
+INSERT INTO answers (descriptions, is_right, questions_id) VALUES ("14", "0", "2");
+INSERT INTO answers (descriptions, is_right, questions_id) VALUES ("100", "1", "3");
+INSERT INTO answers (descriptions, is_right, questions_id) VALUES ("150", "0", "3");
+INSERT INTO answers (descriptions, is_right, questions_id) VALUES ("200", "0", "3");
+INSERT INTO answers (descriptions, is_right, questions_id) VALUES ("45", "1", "4");
+INSERT INTO answers (descriptions, is_right, questions_id) VALUES ("50-5", "1", "4");
+INSERT INTO answers (descriptions, is_right, questions_id) VALUES ("55-10", "1", "4");
+INSERT INTO answers (descriptions, is_right, questions_id) VALUES ("50", "0", "4");
+INSERT INTO answers (descriptions, is_right, questions_id) VALUES ("35+5", "0", "4");
+INSERT INTO answers (descriptions, is_right, questions_id) VALUES ("18", "1", "5");
+INSERT INTO answers (descriptions, is_right, questions_id) VALUES ("19-1", "1", "5");
+INSERT INTO answers (descriptions, is_right, questions_id) VALUES ("17+1", "1", "5");
+INSERT INTO answers (descriptions, is_right, questions_id) VALUES ("20", "0", "5");
+INSERT INTO answers (descriptions, is_right, questions_id) VALUES ("9", "0", "5");
+INSERT INTO answers (descriptions, is_right, questions_id) VALUES ("10", "1", "6");
+INSERT INTO answers (descriptions, is_right, questions_id) VALUES ("9+1", "1", "6");
+INSERT INTO answers (descriptions, is_right, questions_id) VALUES ("11", "0", "6");
+INSERT INTO answers (descriptions, is_right, questions_id) VALUES ("12", "0", "6");
+INSERT INTO answers (descriptions, is_right, questions_id) VALUES ("60", "1", "7");
+INSERT INTO answers (descriptions, is_right, questions_id) VALUES ("59+1", "1", "7");
+INSERT INTO answers (descriptions, is_right, questions_id) VALUES ("70", "0", "7");
+INSERT INTO answers (descriptions, is_right, questions_id) VALUES ("88", "0", "7");
+INSERT INTO answers (descriptions, is_right, questions_id) VALUES ("2", "1", "8");
+INSERT INTO answers (descriptions, is_right, questions_id) VALUES ("1+1", "1", "8");
+INSERT INTO answers (descriptions, is_right, questions_id) VALUES ("3", "0", "8");
+INSERT INTO answers (descriptions, is_right, questions_id) VALUES ("4", "0", "8");
+INSERT INTO answers (descriptions, is_right, questions_id) VALUES ("6", "1", "9");
+INSERT INTO answers (descriptions, is_right, questions_id) VALUES ("5+1", "1", "9");
+INSERT INTO answers (descriptions, is_right, questions_id) VALUES ("8", "0", "9");
+INSERT INTO answers (descriptions, is_right, questions_id) VALUES ("10", "0", "9");
+INSERT INTO answers (descriptions, is_right, questions_id) VALUES ("100", "1", "10");
+INSERT INTO answers (descriptions, is_right, questions_id) VALUES ("99+1", "1", "10");
+INSERT INTO answers (descriptions, is_right, questions_id) VALUES ("120", "0", "10");
+INSERT INTO answers (descriptions, is_right, questions_id) VALUES ("69", "0", "10");
