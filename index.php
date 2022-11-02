a4a02a225b8fe8c54ef238ec1ab488c2f473797d
W4we1 <Wawel2004@gmail.com>
2022-11-02 15:35:01 +0100

    lekcje w czwartek 2.11

--- index.php
+++ index.php
@@ -1,16 +1,22 @@
 <html>
 	<head>
-	<title>quiz</title>
+	<title>Quiz matematyczny</title>
 	</head>
 	<body>
-		<?php
-			$con = new mysqli ("127.0.0.1", "root", " ", "quiz");
-			$res = $con -> query("SELECT * FROM questions");
-
-			$row = $res -> fetch_all(MYSQLI_ASSOC);
-			for($i-0;$i<count($rows);i++){
-			echo $rows[$i]["content"];
-			}
-		?>
+        <div class="pytania">
+            <form method="post">
+        <?php
+        $con=new mysqli("127.0.0.1","root","pass","quiz_matematyczny");
+        $res=$con->query("SELECT id, desription FROM questions");
+        $rows=$res->fetch_all(MYSQLI_ASSOC);
+        for($i=0;$i<count($rows);$i++){
+            echo $rows[$i]["description"].`</br>`;
+            $res2 = $con->query("SELECT");
+            $rows2 = $res2 -> fetch_all(MYSQLI.ASSOC);
+            for ($i=0); 
+        }
+        ?>
+            </form>
+        </div>
 	</body>
 </html>
