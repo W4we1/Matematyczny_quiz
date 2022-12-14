02243ee182580ad1cb908ff0e277ac1a16fe47dc
W4we1 <Wawel2004@gmail.com>
2022-12-14 15:18:00 +0100

    Dodanie plików Brackets

---
+++ Matematyczny_quiz/Wynik.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>QUIZ</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <?php
+            print_r($_POST);
+            $wynik = 0;
+            foreach($_POST as $key=>$value){
+                echo "$key - $value";
+                $wynik = $wynik+$value;
+            };
+            echo $wynik;
+        ?>  
+    </body>
+</html>
